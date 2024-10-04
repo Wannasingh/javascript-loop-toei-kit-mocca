d589aad98ef1a172e3acf7fa4008252aa0f74802
Mocca <p0n-@hotmail.com>
2024-10-04 13:15:43 +0700

    Mocca assignment

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,13 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
-
+minScore = Number.MAX_SAFE_INTEGER;
+console.log(minScore);
+for(let i =0;i<scores.length;i++){
+    if(scores[i]<minScore){
+        minScore = scores[i];
+    }
+    console.log(scores[i]);
+    console.log(minScore);
+}
 console.log(minScore);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,12 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+let j = companyName.length-1;
+for(let i=companyName.length-1;i>=0;i--){
+    
+    console.log(companyName[i] + i);
+    console.log(j-i)
+    reversedCompanyName = reversedCompanyName + companyName[i];
+}
+console.log(reversedCompanyName)
 
-console.log(reversedCompanyName);
