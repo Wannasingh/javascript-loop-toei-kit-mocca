5cfe006fa405a3ce28845df095d172d135ddb008
Kittisak <kittisaksa@gmail.com>
2024-10-04 15:04:33 +0700

    fix:fix more Methods ex-6

--- ex-6.js
+++ ex-6.js
@@ -1,11 +1,16 @@
 // Exercise #6: Reverse the String
 let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
-let companyNameLength = companyName.length - 1;
+
 // Start coding here
 
-for (let num = companyNameLength; num >= 0; num--) {
-  console.log(companyName[num]);
-  reversedCompanyName = reversedCompanyName + companyName[num];
+// for (let num = companyNameLength; num >= 0; num--) {
+//   console.log(companyName[num]);
+//   reversedCompanyName = reversedCompanyName + companyName[num];
+// }
+// console.log(reversedCompanyName);
+
+for (const num in companyName) {
+  reversedCompanyName = companyName[num] + reversedCompanyName;
 }
 console.log(reversedCompanyName);
